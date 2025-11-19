18ca909602dba47cea19fb13e38fa04cabdc9367
lealre <renaanvp@gmail.com>
2025-11-19 20:16:30 +0000

    test: Endpoint to add a user to a group

--- tests/goups_test.go
+++ tests/goups_test.go
@@ -3,9 +3,11 @@ package tests
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"testing"
 
+	"github.com/lealre/movies-backend/internal/api"
 	"github.com/lealre/movies-backend/internal/services/groups"
 	"github.com/lealre/movies-backend/internal/services/users"
 	"github.com/stretchr/testify/require"
@@ -51,6 +53,101 @@ func TestCreateGroup(t *testing.T) {
 		require.Empty(t, respGroupBody.Titles, "titles should be empty")
 		require.NotEmpty(t, respGroupBody.CreatedAt, "createdAt should not be empty")
 		require.NotEmpty(t, respGroupBody.UpdatedAt, "updatedAt should not be empty")
+
+	})
+
+	t.Run("Create a group with wrong user id returns 404", func(t *testing.T) {
+		resetDB(t)
+
+		unknowId := "userId"
+		newGroup := groups.CreateGroupRequest{
+			Name:    "testgroupname",
+			OwnerId: unknowId,
+		}
+		jsonData, err := json.Marshal(newGroup)
+		require.NoError(t, err)
+		respGroup, err := http.Post(
+			testServer.URL+"/groups",
+			"application/json",
+			bytes.NewBuffer(jsonData),
+		)
+		require.NoError(t, err)
+		defer respGroup.Body.Close()
+		require.Equal(t, http.StatusNotFound, respGroup.StatusCode)
+
+		var resp api.ErrorResponse
+		require.NoError(t, json.NewDecoder(respGroup.Body).Decode(&resp))
+		require.Contains(t, resp.ErrorMessage, fmt.Sprintf("id %s not found", unknowId))
+
+	})
+
+}
+
+func TestGroupUsers(t *testing.T) {
+
+	t.Run("Get users from a group successfully", func(t *testing.T) {
+		resetDB(t)
+
+		userOne := users.NewUserRequest{
+			Name:     "testNameOne",
+			Password: "testPass",
+		}
+		userTwo := users.NewUserRequest{
+			Name:     "testNameTwo",
+			Password: "testPass",
+		}
+
+		// Create User 1
+		respUserOne := addUser(t, userOne)
+		defer respUserOne.Body.Close()
+		require.Equal(t, http.StatusCreated, respUserOne.StatusCode)
+		var respUserOneBody users.UserResponse
+		require.NoError(t, json.NewDecoder(respUserOne.Body).Decode(&respUserOneBody))
+
+		// Create User 2
+		respUserTwo := addUser(t, userTwo)
+		defer respUserOne.Body.Close()
+		require.Equal(t, http.StatusCreated, respUserTwo.StatusCode)
+		var respUserTwoBody users.UserResponse
+		require.NoError(t, json.NewDecoder(respUserTwo.Body).Decode(&respUserTwoBody))
+
+		// Create a group for user one
+		newGroup := groups.CreateGroupRequest{
+			Name:    "testgroupname",
+			OwnerId: respUserOneBody.Id,
+		}
+		jsonData, err := json.Marshal(newGroup)
+		require.NoError(t, err)
+		respGroup, err := http.Post(
+			testServer.URL+"/groups",
+			"application/json",
+			bytes.NewBuffer(jsonData),
+		)
+		require.NoError(t, err)
+		defer respGroup.Body.Close()
+		require.Equal(t, http.StatusCreated, respGroup.StatusCode)
+
+		var respGroupBody groups.GroupResponse
+		require.NoError(t, json.NewDecoder(respGroup.Body).Decode(&respGroupBody))
+
+		// Add User 2 to group
+		addUserToGroup := groups.AddUserToGroupRequest{
+			UserId: respUserTwoBody.Id,
+		}
+		jsonData, err = json.Marshal(addUserToGroup)
+		require.NoError(t, err)
+		respGroup, err = http.Post(
+			testServer.URL+"/groups/"+respGroupBody.Id+"/users",
+			"application/json",
+			bytes.NewBuffer(jsonData),
+		)
+		require.NoError(t, err)
+		defer respGroup.Body.Close()
+		require.Equal(t, http.StatusOK, respGroup.StatusCode)
+
+		var respNewUserToGroupBody api.DefaultResponse
+		require.NoError(t, json.NewDecoder(respGroup.Body).Decode(&respNewUserToGroupBody))
+		require.Contains(t, respNewUserToGroupBody.Message, fmt.Sprintf("User %s added to group %s", respUserTwoBody.Id, respGroupBody.Id))
 
 	})
 
